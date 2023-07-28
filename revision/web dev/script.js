f9f8c1ceaf804d577a3c031b6c049ec3992a45e5
Vansh Kumar <vanshgoyal97108@gmail.com>
2023-07-28 19:40:04 +0530

    deleted USELESS FILE

--- revision/web dev/script.js
+++
@@ -1,5 +0,0 @@
- for(let num = 1; num<= 10; num++){
-    let tableOf = 12;  
-  // console.log(tableOf + " * " + num + " = " + tableOf * num);
-  console.log( ` ${tableOf} * ${num} = ${tableOf * num}` );
-}
